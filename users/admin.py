41e7ca8f125d6c36bfc47639de66084d14e8e3cc
ashleynguci <trucanhftu2@gmail.com>
2020-10-12 01:04:39 +0300

    Custom Admin Page

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from . import models
+from django.contrib.auth.admin import UserAdmin
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
